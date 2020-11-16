d66ecefcf2563fa5ce1084e406ad78b9cf4327a0
alexnakov <59810189+alexnakov@users.noreply.github.com>
2020-11-16 15:05:10 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -24,75 +24,93 @@ class Square:
 def check_for_winner():  # Check whether there is a winner, exits the program if there is a winner
     global can_mark, player1_score, player2_score
     if squares[0].symbol == 'X' and squares[1].symbol == 'X' and squares[2].symbol == 'X':
+        can_mark = False
         pygame.time.wait(250)
         pygame.draw.line(root_window, YELLOW, (squares[0].x - 50, squares[0].y + 50),
                          (squares[2].x + 150, squares[2].y + 50), 4)
     elif squares[3].symbol == 'X' and squares[4].symbol == 'X' and squares[5].symbol == 'X':
+        can_mark = False
         pygame.time.wait(250)
         pygame.draw.line(root_window, YELLOW, (squares[3].x - 50, squares[3].y + 50),
                          (squares[5].x + 150, squares[5].y + 50), 4)
     elif squares[6].symbol == 'X' and squares[7].symbol == 'X' and squares[8].symbol == 'X':
+        can_mark = False
         pygame.time.wait(250)
         pygame.draw.line(root_window, YELLOW, (squares[6].x - 50, squares[6].y + 50),
                          (squares[8].x + 150, squares[8].y + 50), 4)
     elif squares[0].symbol == 'X' and squares[3].symbol == 'X' and squares[6].symbol == 'X':
+        can_mark = False
         pygame.time.wait(250)
         pygame.draw.line(root_window, YELLOW, (squares[0].x + 50, squares[0].y - 50),
                          (squares[6].x + 50, squares[6].y + 150), 4)
     elif squares[1].symbol == 'X' and squares[4].symbol == 'X' and squares[7].symbol == 'X':
+        can_mark = False
         pygame.time.wait(250)
         pygame.draw.line(root_window, YELLOW, (squares[1].x + 50, squares[1].y - 50),
                          (squares[7].x + 50, squares[7].y + 150), 4)
     elif squares[1].symbol == 'X' and squares[4].symbol == 'X' and squares[7].symbol == 'X':
+        can_mark = False
         pygame.time.wait(250)
         pygame.draw.line(root_window, YELLOW, (squares[1].x + 50, squares[1].y - 50),
                          (squares[7].x + 50, squares[7].y + 150), 4)
     elif squares[2].symbol == 'X' and squares[5].symbol == 'X' and squares[8].symbol == 'X':
+        can_mark = False
         pygame.time.wait(250)
         pygame.draw.line(root_window, YELLOW, (squares[2].x + 50, squares[2].y - 50),
                          (squares[8].x + 50, squares[8].y + 150), 4)
     elif squares[0].symbol == 'X' and squares[4].symbol == 'X' and squares[8].symbol == 'X':
+        can_mark = False
         pygame.time.wait(250)
         pygame.draw.line(root_window, YELLOW, (squares[0].x - 25, squares[0].y - 25),
                          (squares[8].x + 125, squares[8].y + 125), 4)
     elif squares[2].symbol == 'X' and squares[4].symbol == 'X' and squares[6].symbol == 'X':
+        can_mark = False
         pygame.time.wait(250)
         pygame.draw.line(root_window, YELLOW, (squares[2].x + 125, squares[2].y - 25),
                          (squares[6].x - 25, squares[6].y + 125), 4)
     # Checking if noughts win
     elif squares[0].symbol == 'O' and squares[1].symbol == 'O' and squares[2].symbol == 'O':
+        can_mark = False
         pygame.time.wait(250)
         pygame.draw.line(root_window, YELLOW, (squares[0].x - 50, squares[0].y + 50),
                          (squares[2].x + 150, squares[2].y + 50), 4)
     elif squares[3].symbol == 'O' and squares[4].symbol == 'O' and squares[5].symbol == 'O':
+        can_mark = False
         pygame.time.wait(250)
         pygame.draw.line(root_window, YELLOW, (squares[3].x - 50, squares[3].y + 50),
                          (squares[5].x + 150, squares[5].y + 50), 4)
     elif squares[6].symbol == 'O' and squares[7].symbol == 'O' and squares[8].symbol == 'O':
+        can_mark = False
         pygame.time.wait(250)
         pygame.draw.line(root_window, YELLOW, (squares[6].x - 50, squares[6].y + 50),
                          (squares[8].x + 150, squares[8].y + 50), 4)
     elif squares[0].symbol == 'O' and squares[3].symbol == 'O' and squares[6].symbol == 'O':
+        can_mark = False
         pygame.time.wait(250)
         pygame.draw.line(root_window, YELLOW, (squares[0].x + 50, squares[0].y - 50),
                          (squares[6].x + 50, squares[6].y + 150), 4)
     elif squares[1].symbol == 'O' and squares[4].symbol == 'O' and squares[7].symbol == 'O':
+        can_mark = False
         pygame.time.wait(250)
         pygame.draw.line(root_window, YELLOW, (squares[1].x + 50, squares[1].y - 50),
                          (squares[7].x + 50, squares[7].y + 150), 4)
     elif squares[1].symbol == 'O' and squares[4].symbol == 'O' and squares[7].symbol == 'O':
+        can_mark = False
         pygame.time.wait(250)
         pygame.draw.line(root_window, YELLOW, (squares[1].x + 50, squares[1].y - 50),
                          (squares[7].x + 50, squares[7].y + 150), 4)
     elif squares[2].symbol == 'O' and squares[5].symbol == 'O' and squares[8].symbol == 'O':
+        can_mark = False
         pygame.time.wait(250)
         pygame.draw.line(root_window, YELLOW, (squares[2].x + 50, squares[2].y - 50),
                          (squares[8].x + 50, squares[8].y + 150), 4)
     elif squares[0].symbol == 'O' and squares[4].symbol == 'O' and squares[8].symbol == 'O':
+        can_mark = False
         pygame.time.wait(250)
         pygame.draw.line(root_window, YELLOW, (squares[0].x - 25, squares[0].y - 25),
                          (squares[8].x + 125, squares[8].y + 125), 4)
     elif squares[2].symbol == 'O' and squares[4].symbol == 'O' and squares[6].symbol == 'O':
+        can_mark = False
         pygame.time.wait(250)
         pygame.draw.line(root_window, YELLOW, (squares[2].x + 125, squares[2].y - 25),
                          (squares[6].x - 25, squares[6].y + 125), 4)
@@ -110,7 +128,8 @@ if __name__ == '__main__':
     player_turn = 1
     player1_score, player2_score = 0, 0
     can_mark = True
-    winner = None
+
+    menu = pygame.Surface((700, 700))
 
     cross = pygame.Surface((80, 80))  # Creating the cross picture
     pygame.draw.line(cross, RED, (0, 0), (80, 80), 12)
@@ -143,20 +162,21 @@ if __name__ == '__main__':
                 sys.exit()
             elif event.type == MOUSEBUTTONDOWN:
                 mouse_x, mouse_y = pygame.mouse.get_pos()
-                for square in squares:
-                    if square.symbol:  # Cannot mark a square that has already been marked
-                        continue
-                    if square.collide_point(mouse_x, mouse_y):
-                        if player_turn % 2 == 0:
-                            root_window.blit(nought, (square.x + 10, square.y + 10))
-                            square.symbol = 'O'
-                            pygame.display.update()
-                        else:
-                            root_window.blit(cross, (square.x + 10, square.y + 10))
-                            square.symbol = 'X'
-                            pygame.display.update()
-                        player_turn += 1
-                        check_for_winner()
+                if can_mark:
+                    for square in squares:
+                        if square.symbol:  # Cannot mark a square that has already been marked
+                            continue
+                        if square.collide_point(mouse_x, mouse_y):
+                            if player_turn % 2 == 0:
+                                root_window.blit(nought, (square.x + 10, square.y + 10))
+                                square.symbol = 'O'
+                                pygame.display.update()
+                            else:
+                                root_window.blit(cross, (square.x + 10, square.y + 10))
+                                square.symbol = 'X'
+                                pygame.display.update()
+                            player_turn += 1
+                            check_for_winner()
             elif event.type == KEYDOWN:
                 if event.key == K_0:
                     print_results()
